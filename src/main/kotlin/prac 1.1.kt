933b9c31ad13c479e817879f3f92a3dbfab46243
chand2007 <nidhichauhan21@gnu.ac.in>
2022-08-25 02:59:07 +0530

    Initial commit

---
+++ src/main/kotlin/prac 1.1.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    var int:Int=20
+    var doub:Double=10.0
+    var ft:Float=60.0F
+    var lg:Long=65479852
+    var srt:Short=2
+    var byte:Byte=12
+    var char:Char='S'
+    var bool:Boolean=true
+    var string:String="MAD"
+
+    println("Integer: $int")
+    println("Double: $doub")
+    println("Float: $ft")
+    println("Long: $lg")
+    println("Short: $srt")
+    println("Byte: $byte")
+    println("Char: $char")
+    println("Boolean: $bool")
+    println("String: $string")
+}
